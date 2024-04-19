130a37d348600f40072cc4b21e38493a4cd38348
Mic <michelnativedeveloper@gmail.com>
2024-04-19 23:34:02 +0900

    jetpackcompose practice

--- app/src/main/java/com/example/happybirthdayapps/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayapps/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.example.happybirthdayapps
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.happybirthdayapps.ui.theme.HappyBirthDayAppsTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +38,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage("Android","")
+                    GreetingImage(
+                        "Happy Birthday Mic",
+                        "From My self"
+                    )
                 }
             }
         }
@@ -36,13 +54,59 @@ fun GreetingImage(
     from: String,
     modifier: Modifier = Modifier
 ) {
+    //ダウンロードしたファイル
+    val image = painterResource(R.drawable.androidparty1)
+    Box (modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
 
 }
 
+@Composable
+fun GreetingText(
+    message: String,
+    from: String,
+    modifier: Modifier = Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ){
+        Text(
+            message,
+            Modifier.background(color = Color.Green),//背景色変えてみた
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+       Text(
+           text = from,
+           fontSize = 36.sp,
+           modifier = Modifier
+               .padding(16.dp)
+               .align(alignment = Alignment.End)
+       )
+    }
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     HappyBirthDayAppsTheme {
-        GreetingImage("Android","")
+        GreetingImage(
+            "Happy Birthday Mic",
+            "From My self"
+        )
     }
 }
